fd3d2b60537b3e2a5c25ef67d21c528ca0b98be0
Ian Eddy <ianmseddy@gmail.com>
2020-12-15 20:06:46 +0000

    start fixing age issue

--- fireSense_dataPrepFit.R
+++ fireSense_dataPrepFit.R
@@ -77,7 +77,12 @@ defineModule(sim, list(
                  desc = "RTM without ice/rocks/urban/water. Flammable map with 0 and 1."),
     expectsInput(objectName = 'nonForestedLCCGroups', objectClass = 'list',
                  desc = paste("a named list of non-forested landcover groups",
-                              "e.g. list('wetland' = c(19, 23, 32))")),
+                              "e.g. list('wetland' = c(19, 23, 32))",
+                              'These will become covariates in fireSense_IgnitionFit')),
+    expectsInput(objectName = "nonForest_timeSinceDisturbance", objectClass = "RasterStack", sourceURL = NA,
+                 desc = paste("Two rasters - one is time since disturbance represented annually,",
+                              "the other is a binary form using age < 15 as threshold.",
+                              "Used to track time since fire for non-forest pixels")),
     expectsInput(objectName = "pixelGroupMap2001", objectClass = "RasterLayer", sourceURL = NA,
                  desc = "RasterLayer that defines the pixelGroups for cohortData table in 2001"),
     expectsInput(objectName = "pixelGroupMap2011", objectClass = "RasterLayer",
@@ -662,8 +667,40 @@ plotFun <- function(sim) {
     sim$nonForestedLCCGroups <- list(
       'nonForest_highFlam' = c(16, 17, 18, 19, 22),
       'nonForest_lowFlam' = c(21, 23, 24, 26, 27, 28, 29)
-         ## TODO: what about 30?
     )
+  }
+
+  if (!suppliedElsewhere("nonForest_timeSinceDisturbance")) {
+    #create template of non-forest flammable
+    browser()
+    nonForestFlm <- sim$flammableRTM
+    Flam <- getValues(nonForestFlm)
+    Flam[!is.na(sim$pixelGroupMap2001)] <- NA #don't track forest
+    Flam[!is.na(Flam) & Flam == 0] <- NA #don't track non-flammable
+    nonForestFlm <- setValues(nonForestFlm, Flm)
+
+    #get 15 prior years of fire to initialize
+    earlierFires <- fireSenseUtils::getFirePolygons(years = c(min(P(sim)$fireYears - 15):min(P(sim)$fireYears) - 1),
+                                                    studyArea = sim$studyArea,
+                                                    destinationPath = dPath,
+                                                    useInnerCache = FALSE) %>%
+      rbindlist(.)
+    #need a single sf object
+    earlierFires$timeToAdjust <- min(P(sim)$fireYears) - earlierFires$YEAR
+    #a fire in 1976 will = 15, 1990 = 1.
+    pixelsToAdjust <- fasterize::fasterize(sf = earlierFires,
+                                           field = 'timeToAdjust',
+                                           raster = nonForestFlm,
+                                           fun = 'min') #min = most recent fire
+    rastDT <- data.table('flam' = getValues(nonForestFlm),
+                         'adjAge' = getValues(pixesToAdjust))
+    rstDT[!is.na(flam) & is.na(adjAge), adjAge := 16] #initial age
+    rstDT[, timeClass := ifelse(!is.na(adjAge) & adjAge < 16, 0, 1)]
+    #pixels 15 years or younger since disturbance = 0
+
+    time <- setValues(nonForestFlm, rstDT$adjAge)
+    timeClass <- setValues(nonForestFlm, rstDT$timeClass)
+    sim$nonForest_timeSinceDisturbance <- stack(time, timeClass)
   }
 
   return(invisible(sim))
