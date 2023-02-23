4ec8b9743c54797282ddc9ce02c9549eeb2f6f93
Ian Eddy <ianmseddy@gmail.com>
2023-02-23 11:08:17 -0800

    fsutils version bump

--- fireSense_dataPrepFit.R
+++ fireSense_dataPrepFit.R
@@ -14,7 +14,7 @@ defineModule(sim, list(
   documentation = deparse(list("README.txt", "fireSense_dataPrepFit.Rmd")),
   reqdPkgs = list("data.table", "fastDummies", "ggplot2", "purrr", "SpaDES.tools",
                   "PredictiveEcology/SpaDES.core@development (>= 1.0.6.9016)",
-                  "PredictiveEcology/fireSenseUtils@development (>= 0.0.5.9043)",
+                  "PredictiveEcology/fireSenseUtils@rasterizedFire (>= 0.0.5.9044)",
                   "parallel", "raster", "sf", "sp", "spatialEco", "snow", "terra"),
   parameters = bindrows(
     #defineParameter("paramName", "paramClass", value, min, max, "parameter description"),
