9e9c79253ece8e095d8112a9698eb9754a3be752
Ian Eddy <ianmseddy@gmail.com>
2023-03-03 15:35:11 -0800

    terrarizing the two fxs not in fsutils

--- R/calcNonForestYoungAge.R
+++ R/calcNonForestYoungAge.R
@@ -1,15 +1,25 @@
 calcNonForestYoungAge <- function(landcoverDT, NFTSD, LCCras, cutoffForYoungAge){
+
   nfLCC <- setdiff(colnames(landcoverDT), "pixelID")
   landcoverDT[, sumRows := rowSums(.SD), .SDcol = nfLCC]
   landcoverDT[, age := NFTSD[pixelID]]
   #this need to be chagned in LCCras and also converted to a youngAge raster
   #as the rasters will be aggregated
   pixToChange <- landcoverDT[sumRows > 0 & age < cutoffForYoungAge]$pixelID
+
   landcoverDT[, c("sumRows", "age") := NULL]
+
+  #check how terra works - this should change all pixels if length 2+ spat raster
   LCCras[pixToChange] <- 0
-  youngAge <- raster(LCCras)
-  youngAge[!is.na(LCCras[[1]][])] <- 0 #NA in the landscape must be 0
-  youngAge[pixToChange] <- 1
+  youngAge <- rast(LCCras, nlyr = 1)
+
+  temp <- values(LCCras[[1]])
+  temp[!is.na(temp)] <- 0
+  temp[pixToChange] <- 1
+
+  youngAge <- setValues(youngAge, temp)
+
   LCCras$youngAge <- youngAge
+
   return(LCCras)
 }
--- R/putBackIntoRaster.R
+++ R/putBackIntoRaster.R
@@ -1,10 +1,15 @@
 putBackIntoRaster <- function(lcc, landcoverDT, flammableMap) {
   lccRasters <- list()
   for (i in 1:length(lcc)) {
-    lccRasters[[i]] <- raster(flammableMap)
-    lccRasters[[i]][!is.na(flammableMap)] <- 0 #all non-NA pixels must be 0
-    lccRasters[[i]][landcoverDT$pixelID] <- landcoverDT[, get(lcc[i])]
+    flamMapVals <- values(flammableMap, mat = FALSE)
+    flamMapVals[!is.na(flamMapVals)] <- 0
+    lccRasters[[i]] <- rast(flammableMap)
+    lccVals <- landcoverDT[, get(lcc[i])]
+    flamMapVals[landcoverDT$pixelID] <- lccVals
+    lccRasters[[i]] <- setValues(lccRasters[[i]], flamMapVals)
   }
+  lccRasters <- rast(lccRasters)
   names(lccRasters) <- lcc
+
   return(lccRasters)
 }
