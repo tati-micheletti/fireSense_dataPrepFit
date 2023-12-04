22568e46af171790274ac1d953989643f516679e
Ian Eddy <ianmseddy@gmail.com>
2023-12-04 15:53:30 -0800

    fs utils bump

--- fireSense_dataPrepFit.R
+++ fireSense_dataPrepFit.R
@@ -14,7 +14,7 @@ defineModule(sim, list(
   documentation = deparse(list("README.md", "fireSense_dataPrepFit.Rmd")),
   loadOrder = list(after = c("Biomass_borealDataPrep", "Biomass_speciesParameters")),
   reqdPkgs = list("data.table", "fastDummies",
-                  "PredictiveEcology/fireSenseUtils@development (>= 0.0.5.9055)",
+                  "PredictiveEcology/fireSenseUtils@development (>= 0.0.5.9056)",
                   "ggplot2", "parallel", "purrr", "raster", "sf", "sp",
                   "PredictiveEcology/LandR@development (>= 1.1.0.9073)",
                   "PredictiveEcology/SpaDES.core@development (>= 2.0.2.9006)",
